c37596421d2f41d72b5f19cbaa1b995e4d01eb3b
Jordi Díaz Añorga <jdiaz@plainconcepts.com>
2017-07-20 22:06:21 +0200

    Add project files.

---
+++ Katayuno/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Katayuno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
